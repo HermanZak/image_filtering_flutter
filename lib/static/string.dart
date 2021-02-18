8325b200d7807691e4d1962cd30a3a4dd9114930
HermanZak <germanzakrevskyi@gmail.com>
2021-02-18 20:21:51 +0100

    String constants added

---
+++ lib/static/string.dart
@@ -0,0 +1,5 @@
+class AppStrings {
+  static const String appName = 'Image Filtering Application';
+  static const String noImageSelected = 'No image was selected.';
+  static const String fabPickImageTooltip = 'Pick Image';
+}
